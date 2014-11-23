59d0390451ffe416fc33d894cf5751edd29341ce
kjottmar <kjottmar@gmail.com>
2014-11-23 21:20:31 +0430

    Updated linear regression

--- server.r
+++ server.r
@@ -1,26 +1,39 @@
 library(shiny)
-library(UsingR)
-library(manipulate)
-data(mtcars)
 
+data(mtcars)
+Manuals <- mtcars[mtcars$am==1,]
+Automatics <- mtcars[mtcars$am==0,]
 shinyServer(
       function(input,output){
             output$newHist <- renderPlot({
-                  if (input$variable == "MPG"){
-                        hist(mtcars$mpg, xlab='Miles Per Gallon', col='lightblue',main='Histogram', 
-                             breaks=seq(8,36,by=2))}
+                  if (input$variable == "WT"){
+                        plot(mtcars$wt, mtcars$mpg, type="n", frame=FALSE, xlab="weight, lbs/1000", ylab="MPG")
+                        abline(lm(Manuals$mpg ~ Manuals$wt), col="lightblue")
+                        abline(lm(Automatics$mpg ~ Automatics$wt), col="red")
+                        points(Manuals$wt, Manuals$mpg, pch = 21, bg = "lightblue")
+                        points(Automatics$wt, Automatics$mpg, pch = 21, bg = "red")
+                        legend("topright", inset=c(0,0.2), pch=21, pt.bg=c("lightblue","red"), legend=c("Manual", "Automatic"))}
                   else if (input$variable == "HP"){
-                       hist(mtcars$hp, xlab='Horsepower', col='lightblue',main='Histogram', 
-                            breaks=seq(0,400,by=50))}
+                        plot(mtcars$hp, mtcars$mpg, type="n", frame=FALSE, xlab="horsepower", ylab="MPG")
+                        abline(lm(Manuals$mpg ~ Manuals$hp), col="lightblue")
+                        abline(lm(Automatics$mpg ~ Automatics$hp), col="red")
+                        points(Manuals$hp, Manuals$mpg, pch = 21, bg = "lightblue")
+                        points(Automatics$hp, Automatics$mpg, pch = 21, bg = "red")
+                        legend("topright", inset=c(0,0.2), pch=21, pt.bg=c("lightblue","red"), legend=c("Manual", "Automatic"))}
                   else if (input$variable == "DISP"){
-                        hist(mtcars$disp, xlab='Displacement, cu. in.', col='lightblue',main='Histogram', 
-                             breaks=seq(0,500,by=50))}
+                        plot(mtcars$disp, mtcars$mpg, type="n", frame=FALSE, xlab="displacement, cu. in", ylab="MPG")
+                        abline(lm(Manuals$mpg ~ Manuals$disp), col="lightblue")
+                        abline(lm(Automatics$mpg ~ Automatics$disp), col="red")
+                        points(Manuals$disp, Manuals$mpg, pch = 21, bg = "lightblue")
+                        points(Automatics$disp, Automatics$mpg, pch = 21, bg = "red")
+                        legend("topright", inset=c(0,0.2), pch=21, pt.bg=c("lightblue","red"), legend=c("Manual", "Automatic"))}
                   else if (input$variable == "CYL"){
-                        hist(mtcars$cyl, xlab='Cylinders', col='lightblue',main='Histogram')} 
-                             #breaks=seq(2,6,by=2))}
-                  else if (input$variable == "WT"){
-                        hist(mtcars$wt, xlab='Weight, lbs/1000', col='lightblue',main='Histogram', 
-                             breaks=seq(1,7,by=0.5))}
+                        plot(mtcars$cyl, mtcars$mpg, type="n", frame=FALSE, xlab="horsepower", ylab="MPG")
+                        abline(lm(Manuals$mpg ~ Manuals$cyl), col="lightblue")
+                        abline(lm(Automatics$mpg ~ Automatics$cyl), col="red")
+                        points(Manuals$cyl, Manuals$mpg, pch = 21, bg = "lightblue")
+                        points(Automatics$cyl, Automatics$mpg, pch = 21, bg = "red")
+                        legend("topright", inset=c(0,0.2), pch=21, pt.bg=c("lightblue","red"), legend=c("Manual", "Automatic"))}
             })
 
       }
--- ui.R
+++ ui.R
@@ -4,16 +4,19 @@ shinyUI(pageWithSidebar(
       headerPanel("Vehicle properties from the Motor Trend data set"),
       sidebarPanel(
             selectInput('variable', h4("Car Specifications"), 
-                         c("MPG" = "MPG", "Horsepower" = "HP",
+                         c("Horsepower" = "HP",
                           "Engine Displacement" = "DISP",
                            "Cylinders" = "CYL", "Weight" = "WT")),
       h5("Documentation"),
       "This Shiny app allows you to explore some of the automobile specifications
-      found in the Motor Trend data set. Depending on which specification you would like
-      examine, a different histogram will be produced that shows the distribution for that
-      specification. So, for example, if you choose Horsepower, then the histogram will 
-      show the number of automobiles that have a horsepower falling in a certain range."
+      found in the Motor Trend data set, and how they are related to fuel economy.
+      Depending on which specification you would like to examine, a different 
+      graph will be generated, showing the data points, as well as the linear
+      regression. So, for example, if you choose Horsepower, then the graph will 
+      show the relationship between horsepower and fuel economy for both automatics
+      and manuals transmissions."
       ),
+
       mainPanel(
       plotOutput('newHist')
       )
